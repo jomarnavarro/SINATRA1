d2a059c03a89b58398b2dc1036776b27222a858d
OmarNavarro <onavarro@kueski.com>
2017-11-09 17:50:38 -0600

    Adds datamapper

--- exercise02/main.rb
+++ exercise02/main.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
+#require 'data_mapper'
+require 'dm-core'
+require 'dm-migrations'
 require 'slim'
 
 set :public_folder, 'public'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+class Tool
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :description, Text
+  property :bought_on, Date
+end
+
+DataMapper.finalize
 
 get '/' do
   slim :home 
